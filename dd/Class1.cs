695ff6607935dbfb30e795cba908f46e157699d5
Divya031 <divyasachdeva500@gmail.com>
2022-09-01 04:07:34 -0700

    conditional statements

---
+++ dd/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dd
+{
+    class Class1
+    {
+        public static void main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter age");
+            age = Convert.ToInt32(Console.ReadLine());
+            if(age>60)
+            {
+                Console.WriteLine("Senior Citizen");
+            }
+            else
+            {
+                if(age>18)
+                {
+                    Console.WriteLine("Eligible");
+                }
+                else
+                {
+                    Console.WriteLine("not eligible");
+                }
+
+            }
+        }
+    }
+}
---
+++ dd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace dd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter age");
+            age= Convert.ToInt32( Console.ReadLine( ));
+            if(age>=60)
+            {
+                Console.WriteLine("senior citizen");
+            }
+            else if(age>=18)
+            {
+                    Console.WriteLine("eligible for voting");
+            }
+             else
+             {
+                    Console.WriteLine("Not eligible for voting");
+             }
+            }
+            
+
+        }
+    }
+
