edadb05616ab704c77bdae05307e404982998918
Divya031 <divyasachdeva500@gmail.com>
2022-09-02 05:59:29 -0700

    arrays

---
+++ Divya/Class10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Divya
+{
+    class Class10
+    {
+        public static void main(string[] args)
+        {
+            int[] sample = new int[3];
+            for(int i=0;i<sample.Length;i++)
+            {
+                Console.WriteLine("Value of i[" + i + "]");
+                 sample[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < sample.Length; i++)
+            {
+                Console.Write("After that value is"+i);
+                Console.WriteLine(sample[i]);
+                
+            }
+
+        }
+    }
+}
---
+++ dddotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dddotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
